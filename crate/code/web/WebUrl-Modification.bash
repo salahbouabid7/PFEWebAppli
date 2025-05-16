7e18f9106801009e6a0213fb3089403c1009ac12
salahbouabid7 <112703676+salahbouabid7@users.noreply.github.com>
2025-05-16 18:13:03 +0100

    Update WebUrl-Modification.bash
    modifiat ' to "

--- crate/code/web/WebUrl-Modification.bash
+++ crate/code/web/WebUrl-Modification.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PUBLIC_IP=`curl -s ifconfig.me`
-sed -i  '3  s/localhost/$PUBLIC_IP/'  .env
-sed -i  '4  s/localhost/$PUBLIC_IP/'  .env
+sed -i  "3  s/localhost/$PUBLIC_IP/"  .env
+sed -i  "4  s/localhost/$PUBLIC_IP/"  .env
 
 #Since I am using CMD after ENTRYPOINT, the CMD will be ignored unless it is explicitly handled. One solution is to use exec "$@" inside the ENTRYPOINT script. This is like doing exec npm start, which ensures the CMD is not ignored and gets executed properly.
 exec "$@"
